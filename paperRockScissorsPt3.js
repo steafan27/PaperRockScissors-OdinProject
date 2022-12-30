0880eef2081e89b79328eeb6bfe02379dcda8300
steafanpt0 <steafansteinocher@gmail.com>
2022-12-30 13:57:11 -0600

    Add

---
+++ paperRockScissorsPt3.js
@@ -0,0 +1,42 @@
+console.log("Welcome to Paper Rock Scissors");
+let userChoice;
+let computerChoice;
+let wins = 0;
+let loss = 0;
+let tie = 0;
+let gamePlayed = 0;
+
+while(gamePlayed < 4){
+    userInput = window.prompt().toLowerCase(); 
+    computerChoice = Math.floor(Math.random() * 3); 
+    playRound(computerChoice, userInput);
+    gamePlayed += 1;
+    console.log("Wins", wins, "Loss", loss, "Ties", tie, "Rounds", gamePlayed);
+}
+console.log("FINAL Wins", wins, "Loss", loss, "Ties", tie, "Rounds", gamePlayed);
+
+function playRound(cI, uI){
+    const CHOICES = ["paper", "rock", "scissors"];
+    switch(true){
+        case CHOICES[cI] == "paper" && uI == "paper":
+            return tie += 1;
+        case CHOICES[cI] == "paper" && uI == "rock":
+            return loss += 1;
+        case CHOICES[cI] == "paper" && uI == "scissors":
+            return wins += 1;
+        case CHOICES[cI] == "rock" && uI == "paper":
+            return wins += 1;
+        case CHOICES[cI] == "rock" && uI == "rock":
+            return tie += 1;
+        case CHOICES[cI] == "rock" && uI == "scissors":
+            return loss += 1;
+        case CHOICES[cI] == "scissors" && uI == "paper":
+            return loss += 1;
+        case CHOICES[cI] == "scissors" && uI == "rock":
+            return wins += 1;
+        case CHOICES[cI] == "scissors" && uI == "scissors":
+            return tie += 1;
+    }
+}
+
+
